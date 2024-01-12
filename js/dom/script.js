4c027d32449b0cbc244fe5ea082c90d6d9d84d22
Hkumar-me <141249568+Hkumar-me@users.noreply.github.com>
2024-01-12 16:48:23 +0530

    dom

---
+++ js/dom/script.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+
+// console.log(h1, h2)
+// h1.innerHTML = "<i>harsh</i>"
+// h2.outerHTML = "<i>harsh<i>"
+
+// h1.style.color = "Blue"
+
+// h2.style.backgroundColor = "pink"
+
+
+h1.addEventListener("click", ()=>{
+    // h1.style.scale = 1.2;
+    // h2.style.backgroundColor = "blue"
+}) 
