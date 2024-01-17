05c565b218d35640a9f6fb90831446e7403aa545
Hkumar-me <141249568+Hkumar-me@users.noreply.github.com>
2024-01-17 22:37:39 +0530

    scrollTrigger+locomotiveScroll

--- Tala webpage/script.js
+++ Tala webpage/script.js
@@ -1,22 +1,42 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+
+console.log("hi")
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 
 
 
 
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
 
 
 
 
 
-
-
-
-
-
-
+console.log("hi")
 
 
 
@@ -59,7 +79,7 @@ gsap.to("#page1 img",{
     duration:1,
     scrollTrigger:{
         trigger:"#page1 img",
-        scroller:"body",
+        scroller:"#main",
         // markers:true,
         start:"top 70%",
         end:"top 20%",
@@ -73,7 +93,7 @@ gsap.to("#page2 .textP2 h1",{
     duration:1,
     scrollTrigger:{
         trigger:"#page2 .textP2 h1",
-        scroller:"body",
+        scroller:"#main",
         // markers:true,
         start:"top 80%",
         end:"top 60%",
@@ -90,7 +110,7 @@ gsap.to("#page5 .slider .slider1 h1",{
     // duration:4,
     scrollTrigger:{
         trigger:"#page5",
-        scroller:"body",
+        scroller:"#main",
         scrub:1
     }
 })
@@ -99,7 +119,7 @@ gsap.to("#page5 .slider .slider2 h1",{
     // duration:4,
     scrollTrigger:{
         trigger:"#page5",
-        scroller:"body",
+        scroller:"#main",
         scrub:1
     }
 })
