9157193c1e5517fe8f7856dbb6246410ed92a477
Hkumar-me <141249568+Hkumar-me@users.noreply.github.com>
2024-01-17 16:51:08 +0530

    js

--- Tala webpage/script.js
+++ Tala webpage/script.js
@@ -1,3 +1,27 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let fullScreenMenu = document.querySelector("#full-screen-menu");
 let menu = document.querySelector(".nav i");
 
@@ -56,3 +80,60 @@ gsap.to("#page2 .textP2 h1",{
         scrub:1
     }
 })  
+
+
+
+
+
+gsap.to("#page5 .slider .slider1 h1",{
+    translateX:"-300%",
+    // duration:4,
+    scrollTrigger:{
+        trigger:"#page5",
+        scroller:"body",
+        scrub:1
+    }
+})
+gsap.to("#page5 .slider .slider2 h1",{
+    translateX:"-50%",
+    // duration:4,
+    scrollTrigger:{
+        trigger:"#page5",
+        scroller:"body",
+        scrub:1
+    }
+})
+
+
+
+
+
+
+
+
+
+let yimg = document.querySelector("#page6 .imgMove .yesImg img")
+let yesImg = document.querySelector("#page6 .imgMove .yesImg")
+
+
+let nimg = document.querySelector("#page6 .imgMove .noImg img")
+let noImg = document.querySelector("#page6 .imgMove .noImg")
+
+yesImg.addEventListener("mousemove", (dets)=>{
+    yimg.style.opacity = 1;
+    yimg.style.left = `${(dets.x-100)}px`
+    yimg.style.top = `${(dets.y-500)}px`
+})
+yesImg.addEventListener("mouseleave", (dets)=>{
+    yimg.style.opacity = 0;
+})
+
+noImg.addEventListener("mousemove", (dets)=>{
+    nimg.style.opacity = 1;
+    console.log(dets.x)
+    nimg.style.left = `${(dets.x-900)}px`
+    nimg.style.top = `${(dets.y-500)}px`
+})
+noImg.addEventListener("mouseleave", (dets)=>{
+    nimg.style.opacity = 0;
+})
