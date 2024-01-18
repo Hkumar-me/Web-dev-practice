a12839a755000509b3d9ed5434e7a4e45dc2602a
Hkumar-me <141249568+Hkumar-me@users.noreply.github.com>
2024-01-18 22:15:32 +0530

    gsap

--- final project/script.js
+++ final project/script.js
@@ -13,25 +13,34 @@ tl
     .to("#home #circle #btm img", {
         rotate: "-180deg",
         scale:.5,
-        duration: 1,
         ease: Power1,
         stagger: .1
     }, "eksaath")
     .to("#home #circle #top img", {
         scale:.5,
-        duration: 1,
         ease: Power1,
     }, "eksaath")
     .to("#home #overlay .centerImg .centerInsideImg img", {
         scale: "0",
-        duration: 1,
         ease: Power1
     }, "eksaath")
     .to("#home #overlay .centerImg h1", {
         opacity: "0",
-        duration: 1,
         ease: Power1
     }, "eksaath")
+    .to("#home #overlay .gallery h1", {
+        top:"150%",
+        ease: Power1
+    }, "eksaath")
+    .to("#home #overlay .gola", {
+        top:"50%",
+        scale:"2",
+        ease: Power1
+    }, "eksaath")
+    .to("#home #overlay .gola", {
+        opacity:"0",
+        ease: Power1
+    }, "eksaath2")
     .to("#smCircle", {
         scale:0,
         duration: 1,
